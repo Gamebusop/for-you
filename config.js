5ab5e8e9c37f2438ad2b13e7cb1ef61384f28176
Gamebusop <123640317+Gamebusop@users.noreply.github.com>
2023-11-06 00:22:54 +0700

    init file

---
+++ config.js
@@ -0,0 +1,12 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hello ! 🖐🖐🖐',
+    introDesc: `Anh có điều này muốn nói với Em.`,
+    btnIntro: 'OK',
+    desc: '...Tuy thời gian chúng ta bên nhau là rất ngắn ngủi, có những lúc buồn lúc vui lúc giận. Nhưng tình cảm anh dành cho em như một loại đam mê vậy. Nó khiến anh phải kiên trì, nỗ lực và không bao giờ bỏ cuộc để luôn giữ lấy nó trong tim.Vậy nên, liệu chúng ta có thể quay trở lại được không? Để bắt đầu một cuộc tình mới! Ở một thế giới mà chỉ có hai ta cùng đồng hành với nhau và sẽ không ai có thể phá huỷ đi thế giới đó bởi vì anh là tấm khiên bảo vệ thế giới ấy!!Còn Em, Linh câu trả lời của em sẽ là ?',
+    btnYes: 'Hãy cho anh biết đáp án của Em!',
+    reasonPlaceholder: 'Bấm nút bên dưới.',
+    btnReply: '->',
+    btnAccept: 'Viết câu trả lời ở đây',
+    messLink: 'https://www.facebook.com/profile.php?id=100087744113172'
+}
